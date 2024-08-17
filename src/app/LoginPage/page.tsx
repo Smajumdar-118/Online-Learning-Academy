446beb0215abe743b8270f73de640e1327bb9300
Sayan <sayanmajumdar4444@gmail.com>
2024-08-17 21:56:03 +0530

    Frontend Completed

--- src/app/LoginPage/page.tsx
+++ src/app/LoginPage/page.tsx
@@ -13,7 +13,7 @@ import {
 import Link from "next/link";
 
 
-export default function SignupFormDemo() {
+export default function LoginFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -30,21 +30,12 @@ export default function SignupFormDemo() {
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to Aceternity
       </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Please Signup to save your progress and activity and make your profile to showcase
+      <p className="text-neutral-600 text-lg max-w-sm mt-2 dark:text-neutral-300">
+        Please login to check your progress and activity in your profile
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Sayan" type="text" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Majumdar" type="text" />
-          </LabelInputContainer>
-        </div>
+        
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
@@ -59,7 +50,7 @@ export default function SignupFormDemo() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          Log In &rarr;
           <BottomGradient />
         </button>
 
@@ -72,7 +63,7 @@ export default function SignupFormDemo() {
           >
             <IconUser  className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              <Link href="/LoginPage">Login</Link>
+              <Link href='/SignupPage'>Sign Up</Link>
             </span>
             <BottomGradient />
           </button>
