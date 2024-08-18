8bf6cd39329992f499a1bdd433ce6475dfc9d0e7
Sayan <sayanmajumdar4444@gmail.com>
2024-08-19 01:52:00 +0530

    fixing vercel connection

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose';
 
-export async function Connect(){
+export async function Connect() {
     try {
-        mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI!);
-        const  connection = mongoose.connection
+        const uri = process.env.NEXT_PUBLIC_MONGO_URI;
+        if (!uri) {
+            throw new Error("MongoDB URI is undefined. Check environment variables.");
+        }
+        mongoose.connect(uri);
+        const connection = mongoose.connection;
 
-        connection.on('connected' , ()=>{
+        connection.on('connected', () => {
             console.log("MongoDB Connected");
-        })
+        });
 
-        connection.on('error' , (err : any)=>{
-            console.log("Database Connection Error "+ err);
+        connection.on('error', (err: any) => {
+            console.log("Database Connection Error " + err);
             process.exit();
-        })
+        });
 
     } catch (error) {
         console.log("Connection Failed!!" + error);
