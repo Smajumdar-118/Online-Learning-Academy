0551f41ce58e39071d9ab0794578f0b80019a9fd
Sayan <sayanmajumdar4444@gmail.com>
2024-08-21 01:31:00 +0530

    Improved Designing

--- src/app/verifyPage/page.tsx
+++ src/app/verifyPage/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { motion } from "framer-motion";
+import { Button } from '@/components/ui/moving-border'
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function VerificationPage() {
@@ -16,7 +18,7 @@ export default function VerificationPage() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.3 }}
-          className="mb-8"
+          className="mb-1"
         >
           <Image
             src="https://cdn-icons-png.flaticon.com/512/190/190411.png"
@@ -51,9 +53,9 @@ export default function VerificationPage() {
           transition={{ delay: 0.9 }}
           className="mt-8"
         >
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-lg hover:bg-blue-600">
-            Back to Home
-          </button>
+          <Button className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800 text-xl">
+           <Link href="/">Back to Home</Link>
+          </Button>
         </motion.div>
       </motion.div>
     </div>
